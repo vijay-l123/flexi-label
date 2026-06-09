62bd8ad75d816e51c5f3e0d2ed89e6a779620e49
MohammadShoaib0059 <mohammadshoaib9689@gmail.com>
2026-06-09 11:52:57 +0530

    stg files

--- src/Services/config.ts
+++ src/Services/config.ts
@@ -1,5 +1,6 @@
-const baseUrl = "http://194.113.194.151:8080/";
-// const baseUrl = "http://194.113.194.151:1011/";
+// const baseUrl = "http://194.113.194.151:8080/";
+const baseUrl = "http://194.113.194.151:1011/";
+// const baseUrl = "http://FS330:8080/FlexielableApi/";
 
 const config = {
   baseUrl: baseUrl,
